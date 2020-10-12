58dd969bb000181ae5540757b1a364c582263b6c
Robson Barbosa Souza <powerrob92@gmail.com>
2020-10-12 20:41:02 -0300

    chore: ajust details of landing page

--- happy-web/src/pages/landing/index.tsx
+++ happy-web/src/pages/landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
 
 import './landing.css';
 
@@ -13,6 +14,11 @@ const Landing: React.FC = () => {
           <h1>Leve felicidade para o mundo</h1>
           <p>Visite orfanatos e mude o dia de muitas crianças.</p>
         </main>
+        <div className="location">
+          <strong>Luziânia</strong>
+          <span>Goiás</span>
+        </div>
+        <a href="/" className="enter-app"><FiArrowRight size={32}/></a>
       </div>
     </div>
   );
