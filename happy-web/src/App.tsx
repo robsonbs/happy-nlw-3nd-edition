7e4de1b3ed056c3fb4f6d7110046dde47ed2cad3
Robson Barbosa Souza <powerrob92@gmail.com>
2020-10-12 19:48:45 -0300

    chore: start web project landingpage

---
+++ happy-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Landing from './pages/landing';
+
+import './styles/global.css';
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
---
+++ happy-web/src/pages/landing/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './landing.css';
+
+import logoImg from '../../images/logo.svg';
+
+const Landing: React.FC = () => {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
