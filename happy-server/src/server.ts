fba6f7e92619e9e9395ba00654bcdeb27e9c496c
Robson Barbosa Souza <powerrob92@gmail.com>
2020-10-14 10:20:03 -0300

    chore: add cors to allow access from web front

--- happy-server/src/server.ts
+++ happy-server/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
+
 import 'express-async-errors';
 
 import './database/connection';
@@ -8,13 +10,12 @@ import routes from './routes';
 import errorHandler from './errors/handler';
 
 const app = express();
-
+app.use(cors())
 app.use(express.json())
-
 app.use(routes)
-
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler)
+
 app.listen(3333, () => {
   console.log("🚀server start on 3333!")
 })
