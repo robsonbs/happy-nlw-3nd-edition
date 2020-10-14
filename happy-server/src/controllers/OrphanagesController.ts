777b830a392a0eedb54a6a520fd6630fc4875b5a
Robson Barbosa Souza <powerrob92@gmail.com>
2020-10-14 10:19:19 -0300

    feat: add validation rules on create new orphanage

--- happy-server/src/controllers/OrphanagesController.ts
+++ happy-server/src/controllers/OrphanagesController.ts
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import { request, Request, Response } from "express";
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
@@ -35,7 +36,7 @@ export default {
     const requestImages = req.files as Express.Multer.File[];
     const images = requestImages.map(image => { return { path: image.filename } })
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -44,7 +45,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false
+    });
+
+    const orphanage = orphanagesRepository.create(data);
 
     await orphanagesRepository.save(orphanage);
 
