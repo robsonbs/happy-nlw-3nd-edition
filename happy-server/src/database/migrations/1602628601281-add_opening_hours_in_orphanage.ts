d868924feeb9d28bf11f330e5b113aba2f1f775e
Robson Barbosa Souza <powerrob92@gmail.com>
2020-10-13 21:41:06 -0300

    chore: create table orphanage

---
+++ happy-server/src/database/migrations/1602628601281-add_opening_hours_in_orphanage.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class addOpeningHoursInOrphanage1602628601281 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('orphanages', new TableColumn({
+            name: "opening_hours",
+            type: "varchar"
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('orphanages','opening_hours');
+    }
+
+}
