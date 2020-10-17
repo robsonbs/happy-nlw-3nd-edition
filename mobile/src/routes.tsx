f12e027357228d53ddab69003f8b9d4545160fc5
Robson Barbosa Souza <powerrob92@gmail.com>
2020-10-17 07:26:26 -0300

    fix: incompatibilites of data structure

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -3,7 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import OrphanagesMap from './pages/OrphanagesMap';
-import OrphanagesDetails from './pages/OrphanagesDetails';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import OrphanageDetails from './pages/OrphanageDetails';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -12,7 +14,9 @@ const Routes: React.FC = () => {
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="OrphanagesMap" component={OrphanagesMap} />
-        <Screen name="OrphanageDetails" component={OrphanagesDetails} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+        <Screen name="SelectMapPosition" component={SelectMapPosition} />
+        <Screen name="OrphanageData" component={OrphanageData} />
       </Navigator>
     </NavigationContainer>
   );
